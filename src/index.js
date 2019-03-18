8b9b767fed579f4abdcadb79b0cafaaa9e13ce16
bichaorui <abcd_627360647@qq.com>
2019-03-18 23:59:36 +0800

    webpack/react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
+
+import Main from './router/index'
+
+ReactDOM.render(
+  <Main/>,
+  document.getElementById("root")
+);
