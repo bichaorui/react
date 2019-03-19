29902b24720c7d8a7c552ec9c8d6e02bad67544a
毕晁瑞 <wx-bicr@dtdream.com>
2019-03-19 16:01:26 +0800

    add address

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,16 +2,20 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
+const getAdress = require('./address');
 
 module.exports = merge(common, {
 	mode: 'development',
 	devtool: 'inline-source-map',
 	devServer: {
+		contentBase: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
+		historyApiFallback: true,
 		// contentBase: './dist',
-    contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
-    hot: true
+		host: getAdress(),
+		hot: true
 	},
   plugins: [
     new webpack.HotModuleReplacementPlugin()
