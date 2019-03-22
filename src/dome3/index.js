c1801579fe80fd93fdfc087fde8e043b65f2b8be
毕晁瑞 <wx-bicr@dtdream.com>
2019-03-22 20:02:58 +0800

    demo3

---
+++ src/dome3/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {useState, useEffect, useContext} from 'react';
+
+export default () => {
+	return (
+		<div>
+			123
+		</div>
+	)
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import ReactDOM from 'react-dom';
 import React, {Component} from 'react';
 
 // import Main from './router/index'
-import Main from './router2/index'
+// import Main from './router2/index'
+import Main from './dome3/index'
 
 ReactDOM.render(
   <Main/>,
